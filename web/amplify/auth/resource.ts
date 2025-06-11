b361de1fe9ee9fa1f4e03c4116d51d83d9ef3f33
Truong Giang <giangfnptit@gmail.com>
2025-06-12 01:13:46 +0700

    Temporary remove defineAuth

--- web/amplify/auth/resource.ts
+++ web/amplify/auth/resource.ts
@@ -4,24 +4,26 @@ import { defineAuth } from '@aws-amplify/backend'
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
-export const auth = defineAuth({
-  loginWith: {
-    email: {
-      verificationEmailStyle: 'LINK',
-      verificationEmailSubject: 'Welcome to my app!',
-      verificationEmailBody: (createLink) =>
-        `Your verification link is ${createLink()}.`
-    }
-  },
-  userAttributes: {
-    phoneNumber: {
-      required: false
-    },
-    givenName: {
-      required: true
-    },
-    familyName: {
-      required: true
-    }
-  }
-})
+// export const auth = defineAuth({
+//   loginWith: {
+//     email: {
+//       verificationEmailStyle: 'LINK',
+//       verificationEmailSubject: 'Welcome to my app!',
+//       verificationEmailBody: (createLink) =>
+//         `Your verification link is ${createLink()}.`
+//     }
+//   },
+//   userAttributes: {
+//     phoneNumber: {
+//       required: false
+//     },
+//     givenName: {
+//       required: true
+//     },
+//     familyName: {
+//       required: true
+//     }
+//   }
+// })
+
+export const auth = {}
